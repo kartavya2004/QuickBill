0fa79c15a805d0051b7a4d5689a0aedb70174a57
kartavya2004 <97383526+kartavya2004@users.noreply.github.com>
2025-03-25 16:04:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import InvoiceForm from "./components/InvoiceForm";
+import React from 'react';
+import { SnackbarProvider } from 'notistack'; // Import SnackbarProvider
+import InvoiceModal from './components/InvoiceModal'; // Ensure this is correctly imported if used
 
 const App = () => {
   return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm />
-      </Container>
-    </div>
+    <SnackbarProvider maxSnack={3}> {/* Wrap your application with SnackbarProvider */}
+      <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+        <Container>
+          <InvoiceForm />
+        </Container>
+      </div>
+    </SnackbarProvider>
   );
 };
 
